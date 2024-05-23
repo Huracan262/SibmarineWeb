00800c4f6e159e464200e00fbf544ba3974a914a
Grigore <voytov@mssc.su>
2024-05-23 11:18:29 +0700

    feat: add Clients block style

--- src/routes/IndexPage/views/Clients/Clients.tsx
+++ src/routes/IndexPage/views/Clients/Clients.tsx
@@ -2,9 +2,13 @@ import React from 'react';
 
 import useBem from '@steroidsjs/core/hooks/useBem';
 
+import Marquee from 'react-fast-marquee';
+
+import Wrapper from 'shared/Wrapper';
+import Title from 'ui/Title';
+import CLIENTS_LIST from 'data/CLIENTS_LIST';
+
 import './Clients.scss';
-import Wrapper from '../../../../shared/Wrapper';
-import Title from '../../../../ui/Title';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IClients {
@@ -15,11 +19,41 @@ const Clients: React.FC<IClients> = ({...props}) => {
     const bem = useBem('Clients');
 
     return (
-        <div className={bem.block()}>
+        <section className={bem.block()}>
             <Wrapper>
                 <Title>Наши клиенты</Title>
+
+                <div className={bem.element('container')}>
+                    <p className={bem.element('description')}>
+                        Клиенты - основа любой компании. Именно благодаря им мы растём,
+                        развиваемся и делаем нашу компанию лучше каждый день. Мы гордимся тем,
+                        что помогаем нашим клиентам достигать своих целей и решать различные задачи.
+                    </p>
+
+                    <ul className={bem.element('list')}>
+                        <Marquee
+                            direction="right"
+                            speed={30}
+                            gradient
+                            gradientColor="#162639"
+                            style={{overflow: 'clip'}}
+                        >
+                            {CLIENTS_LIST.map((item, index) => (
+                                <li
+                                    className={bem.element('item')}
+                                    key={index}
+                                >
+                                    <img
+                                        src={item.src}
+                                        alt={item.title}
+                                    />
+                                </li>
+                            ))}
+                        </Marquee>
+                    </ul>
+                </div>
             </Wrapper>
-        </div>
+        </section>
     );
 };
 
