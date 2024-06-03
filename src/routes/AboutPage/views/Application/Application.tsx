bd43be7f8519681689d308d05a6af9ea3aa34293
Grigore <voytov@mssc.su>
2024-06-03 13:21:32 +0700

    feat: add Application block

--- src/routes/AboutPage/views/Application/Application.tsx
+++ src/routes/AboutPage/views/Application/Application.tsx
@@ -7,8 +7,8 @@ import Theme from '../../../../enums/Theme';
 import getWhiteModifier from '../../../../utils/getWhiteModifier';
 import Wrapper from '../../../../shared/Wrapper';
 import SOCIALS_LIST from '../../../../data/SOCIALS_LIST';
+import Button from '../../../../ui/Button';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-interface
 interface IApplication {
     theme: Theme,
 }
@@ -18,21 +18,21 @@ const Application = ({theme}: IApplication) => {
 
     return (
         <article className={getWhiteModifier(bem.block(), theme)}>
-            <Wrapper>
+            <Wrapper className={bem.element('wrapper')}>
                 <h2 className={bem.element('title')}>Напишите нам</h2>
 
                 <div className={bem.element('container')}>
                     <form className={bem.element('form')}>
                         <div className={bem.element('block-input')}>
                             <label
-                                className={'visually-hidden'}
+                                className='visually-hidden'
                                 htmlFor="name"
                             >
                                 Ваше имя:
                             </label>
 
                             <input
-                                className={bem.element('')}
+                                className={bem.element('input')}
                                 type="text"
                                 name="name"
                                 id="name"
@@ -43,27 +43,62 @@ const Application = ({theme}: IApplication) => {
                         </div>
 
                         <div className={bem.element('block-input')}>
-                            <label className={'visually-hidden'} htmlFor="contacts">Контакты:</label>
-                            <input className={bem.element('')} type="text" name="contacts" id="contacts" maxLength="50" required
-                                   placeholder="Email / телефон"/>
+                            <label
+                                className='visually-hidden'
+                                htmlFor="contacts"
+                            >
+                                Контакты:
+                            </label>
+
+                            <input
+                                className={bem.element('input')}
+                                type="text"
+                                name="contacts"
+                                id="contacts"
+                                maxLength={50}
+                                required
+                                placeholder="Email / телефон"
+                            />
                         </div>
 
                         <div className={bem.element('block-input')}>
-                            <label className={'visually-hidden'} htmlFor="text">Заявка:</label>
-                            <input className={bem.element('')} type="text" name="text" id="text" maxLength="200" required
-                                   placeholder="Введите ваш запрос"/>
+                            <label
+                                className='visually-hidden'
+                                htmlFor="text"
+                            >
+                                Заявка:
+                            </label>
+
+                            <input
+                                className={bem.element('input')}
+                                type="text"
+                                name="text"
+                                id="text"
+                                maxLength={200}
+                                required
+                                placeholder="Введите ваш запрос"
+                            />
                         </div>
 
-                        <button className={bem.element('')} type="submit">Связаться с нами</button>
+                        <Button
+                            className={bem.element('button')}
+                            type="submit"
+                        >
+                            Связаться с нами
+                        </Button>
 
-                        <ul className={bem.element('')}>
+                        <ul className={bem.element('socials-list')}>
                             {SOCIALS_LIST.map((item, index) => (
                                 <li
-                                    className={bem.element('')}
+                                    className={bem.element('socials-item')}
                                     key={index}
                                 >
-                                    <a className={bem.element('')} href={item.url}>
+                                    <a
+                                        className={bem.element('social-link')}
+                                        href={item.url}
+                                    >
                                         {item.logo}
+
                                         <span className="visually-hidden">{item.name}</span>
                                     </a>
                                 </li>
@@ -71,10 +106,9 @@ const Application = ({theme}: IApplication) => {
                         </ul>
                     </form>
 
-                    <p className={bem.element('')}>
-                        Здесь будет картинка
+                    <p className={bem.element('picture')}>
                         <img
-                            className={bem.element('')}
+                            className={bem.element('img')}
                             src=""
                             alt=""
                         />
--- src/ui/Button/Button.tsx
+++ src/ui/Button/Button.tsx
@@ -9,15 +9,17 @@ interface IButton {
     children: string,
     onClick?: () => void,
     className?: string,
+    type?: 'button' | 'submit',
     disabled?: boolean,
 }
 
-const Button: React.FC<IButton> = ({children, onClick, className, disabled = false}: IButton) => {
+const Button: React.FC<IButton> = ({children, onClick, className, disabled = false, type = 'button'}: IButton) => {
     const bem = useBem('Button');
 
     return (
         <button
             className={`${bem.block()} ${className}`}
+            type={type}
             onClick={onClick}
             disabled={disabled}
         >
